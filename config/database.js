894424d1548631cdc741a1a03be6164c496f6c47
Chris Liang <chrisliang98@gmail.com>
2016-06-08 12:34:53 -0400

    oauth finall yworks

--- config/database.js
+++ config/database.js
@@ -2,4 +2,4 @@ module.exports = {
 
     'url' : 'mongodb://localhost:27017/test' //mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
-};}
+};
--- server.js
+++ server.js
@@ -1,44 +1,27 @@
 var express = require('express');
 var app = express();
 
-//lets require/import the mongodb native drivers.
-var mongodb = require('mongodb');
-
-//We need to work with "MongoClient" interface in order to connect to a mongodb server.
-var MongoClient = mongodb.MongoClient;
-
-// Connection URL. This is where your mongodb server is running.
-var url = 'mongodb://localhost/test';
-
 var port = process.env.PORT || 8000;
+var mongoose = require('mongoose');
 var passport = require('passport');
 var flash = require('connect-flash');
 
 var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
+var session      = require('express-session');
 
+var configDB = require('./config/database.js');
 
-MongoClient.connect(url, function (err, db) {
-    if (err) {
-        console.log('Unable to connect to the mongoDB server. Error:', err);
-    } else {
-        //HURRAY!! We are connected. :)
-        console.log('Connection established to', url);
-        // do some work here with the database.            
-        //Close connection
-        db.close();
-    }
-});
+app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs');
 
-var session = require('express-session');
 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 //connect to database file (which contains db url)
-
-
+mongoose.connect(configDB.url);
 require('./config/passport')(passport);
 app.use(morgan('dev')); //log every rq to the console
 app.use(cookieParser()); // read cookies (needed for auth)
@@ -53,38 +36,26 @@ app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
 //routes
-require('./app/routes.js')(app, passport); // load routes and pass in app + configured passport
+require('./app/routes.js')(app, passport); // loud routes and pass in app + configured passport
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-        if (msg.length > 0) {
-            io.emit('chat message', msg);	 
-            MongoClient.connect(url, function (err, db) {
-                if (err) {
-                    console.log('Unable to connect to the mongoDB server. Error:', err);
-                } else {
-                    //HURRAY!! We are connected. :)
-                    console.log('Adding messages to db');
-                    // do some work here with the database.    
-                    var messages = db.collection('messages');
-
-                    //Close connection
-                    db.close();
-                }
-            });    
-        };
-    });
+		socket.on('chat message', function(msg){
+				if (msg.length > 0) {
+						io.emit('chat message', msg);	  
+				};
+		});
 });
 
 io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
-    });
+		console.log('a user connected');
+		socket.on('disconnect', function(){
+				console.log('user disconnected');
+		});
 });
+
 ///// launch
 http.listen(8000, function(){
-    console.log('listening on *:8000');
-});
+ 		console.log('listening on *:8000');
+ });
 
