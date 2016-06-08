9cd1e75516a9b17730887d326cd18ba7524ffb66
Chris Liang <chrisliang98@gmail.com>
2016-06-08 12:04:47 -0400

    readded some files

---
+++ config/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    'googleAuth' : {
+        'clientID'      : '154549300860-iola5t3o5kt9q79mj5rmmpcnq3hgnoc7.apps.googleusercontent.com',
+        'clientSecret'  : '3zHSq6p9jkzwDS7DWl46vtQl',
+        'callbackURL'   : 'http://localhost:8000/auth/google/callback'
+    }
+
+};
---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+    'url' : 'mongodb://localhost:27017/test' //mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};}
