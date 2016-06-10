fb12a86811b679c8527128e737b609531cdb2f7b
Justin Pacquing <pacqqu@gmail.com>
2016-06-10 05:46:16 -0400

    updated pages to have sign-in & out button, made a form for posting

--- app/routes.js
+++ app/routes.js
@@ -23,20 +23,20 @@ module.exports = function(app, passport) {
  
 
     app.get('/', function(req, res) {
-        res.render('index.ejs');
-    });
+				res.render('index.ejs');
+		});
 
-    app.get('/login', function(req, res) {
-        res.render('login.ejs'); //load login.ejs files
-    });
+		app.get('/login', function(req, res) {
+				res.render('login.ejs'); //load login.ejs files
+		});
 
-    app.get('/dashboard', isLoggedIn, function(req, res) {
-        res.render('dashboard.ejs', {
-            user : req.user // get the user out of session and pass to template
-        });
-    });
+		app.get('/dashboard', isLoggedIn, function(req, res) {
+				res.render('dash.ejs', {
+						user : req.user // get the user out of session and pass to template
+				});
+		});
 
-    app.get('/chat', isLoggedIn, function(req, res) {
+		app.get('/chat', isLoggedIn, function(req, res) {
 				message.find(function (err, messages) {
 						if (err) {
 								return console.error(err);
@@ -52,38 +52,38 @@ module.exports = function(app, passport) {
 						}
 				});
 				res.render('messages.ejs', {
-            user : req.user, // get the user out of session and pass to template
-            m : html_string
-        });
-    });
+						user : req.user, // get the user out of session and pass to template
+						m : html_string
+				});
+		});
 
-    app.get('/files', function(req, res) {
-        res.render('files.ejs', {
-            user : req.user // get the user out of session and pass to template
-        });
-    });
+		app.get('/files', function(req, res) {
+				res.render('files.ejs', {
+						user : req.user // get the user out of session and pass to template
+				});
+		});
 
-    // route for logging out
-    app.get('/logout', function(req, res) {
-        req.logout();
-        res.redirect('/');
-    });
+		// route for logging out
+		app.get('/logout', function(req, res) {
+				req.logout();
+				res.redirect('/');
+		});
 
-    app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+		app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
 
-    // the callback after google has authenticated the user
-    app.get('/auth/google/callback',
-            passport.authenticate('google', {
-                successRedirect : '/dashboard',
-                failureRedirect : '/'
-            }));
+		// the callback after google has authenticated the user
+		app.get('/auth/google/callback',
+						passport.authenticate('google', {
+								successRedirect : '/dashboard',
+								failureRedirect : '/'
+						}));
 };
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
-    // if user is authenticated in the session, carry on
-    if (req.isAuthenticated())
-        return next();
+		// if user is authenticated in the session, carry on
+		if (req.isAuthenticated())
+				return next();
 
-    // if they aren't redirect them to the home page
-    res.redirect('/');
+		// if they aren't redirect them to the home page
+		res.redirect('/');
 }
