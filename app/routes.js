f2854fcf840c6c8393873b828df5cdcf62e9ee30
Chris Liang <chrisliang98@gmail.com>
2016-06-10 12:21:24 -0400

    routing / intropage

--- app/routes.js
+++ app/routes.js
@@ -4,7 +4,7 @@ module.exports = function(app, passport) {
 
     var mongoose = require('mongoose');
     var message = require('../app/msg_model.js');
-		var posts = require('../app/post_model.js');
+    var posts = require('../app/post_model.js');
     var configDB = require('../config/database.js');
 
     //connect to database file (which contains db url)
@@ -18,91 +18,103 @@ module.exports = function(app, passport) {
     });
 
     var html_string = "";
-		var pst_string = "";
+    var pst_string = "";
     // Base code for querying everything in the database
     // Fetches previous messages and formats them into html_string
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-		
+
 
     app.get('/', function(req, res) {
-				res.render('index.ejs');
-		});
+        if (logged) {
+            res.render('index.ejs', {
+                user : req.user
+            });
+        }
+        else {
+            res.render('intro.ejs');
+        }
+    });
 
-		app.get('/login', function(req, res) {
-				res.render('login.ejs'); //load login.ejs files
-		});
+    app.get('/login', function(req, res) {
+        res.render('login.ejs'); //load login.ejs files
+    });
 
-		app.get('/dashboard', isLoggedIn, function(req, res) {
-				posts.find(function (err, psts) {
-						if (err) {
-								return console.error(err);
-						}
-						//console.log("start");
-						//console.log(psts);
-						// console.log(messages.length);
-						pst_string = "";
-						for (i=0;i<psts.length;i++){
-								pst_string += '<div class="ui left aligned segment"><a class="ui blue ribbon label">Class Post</a><span></span><!--<h3 class="writing">Rick Melucci</h3>--><p class="post">' + psts[i]['post'] + '</p></div><br><br><br>';
-								/*'<div class="ui left floated segment center aligned chat-bubble">' + '<p>'+ psts[i]['post'] + '</p>' + '</div>';*/
-								//console.log("hey");
-								//console.log(pst_string);
-						}
-				});
-				res.render('dash.ejs', {
-						user : req.user, // get the user out of session and pass to template
-				p : pst_string
-		});
-});
-		
-app.get('/chat', isLoggedIn, function(req, res) {
-		//html_string = "";
-		message.find(function (err, messages) {
-				if (err) {
-						return console.error(err);
-				}
-				// console.log(messages);
-				// console.log(messages.length);
-				for (i=0;i<messages.length;i++){
-						if (messages[i]['name'] == "Bob"){ //req.user.username ){ // checks if session matches message
-								html_string += '<div class="ui left floated segment center aligned chat-bubble">' + '<p>'+ messages[i]['message'] + '</p>' + '</div>';
-						} else {
-								html_string += '<div class="ui left floated segment center aligned chat-bubble">' + '<p>'+ messages[i]['message'] + '</p>' + '</div>';
-						}
-				}
-		});
-		res.render('messages.ejs', {
-				user : req.user, // get the user out of session and pass to template
-				m : html_string
-		});
-});
+    app.get('/dashboard', isLoggedIn, function(req, res) {
+        posts.find(function (err, psts) {
+            if (err) {
+                return console.error(err);
+            }
+            //console.log("start");
+            //console.log(psts);
+            // console.log(messages.length);
+            pst_string = "";
+            for (i=0;i<psts.length;i++){
+                pst_string += '<div class="ui left aligned segment"><a class="ui blue ribbon label">Class Post</a><span></span><!--<h3 class="writing">Rick Melucci</h3>--><p class="post">' + psts[i]['post'] + '</p></div><br><br><br>';
+                /*'<div class="ui left floated segment center aligned chat-bubble">' + '<p>'+ psts[i]['post'] + '</p>' + '</div>';*/
+                //console.log("hey");
+                //console.log(pst_string);
+            }
+        });
+        res.render('dash.ejs', {
+            user : req.user, // get the user out of session and pass to template
+            p : pst_string
+        });
+    });
 
-app.get('/files', function(req, res) {
-		res.render('files.ejs', {
-				user : req.user // get the user out of session and pass to template
-		});
-});
+    app.get('/chat', isLoggedIn, function(req, res) {
+        //html_string = "";
+        message.find(function (err, messages) {
+            if (err) {
+                return console.error(err);
+            }
+            // console.log(messages);
+            // console.log(messages.length);
+            for (i=0;i<messages.length;i++){
+                if (messages[i]['name'] == "Bob"){ //req.user.username ){ // checks if session matches message
+                    html_string += '<div class="ui left floated segment center aligned chat-bubble">' + '<p>'+ messages[i]['message'] + '</p>' + '</div>';
+                } else {
+                    html_string += '<div class="ui left floated segment center aligned chat-bubble">' + '<p>'+ messages[i]['message'] + '</p>' + '</div>';
+                }
+                }
+            });
+    res.render('messages.ejs', {
+        user : req.user, // get the user out of session and pass to template
+        m : html_string
+    });
+        });
 
-// route for logging out
-app.get('/logout', function(req, res) {
-		req.logout();
-		res.redirect('/');
-});
+        app.get('/files', function(req, res) {
+            res.render('files.ejs', {
+                user : req.user // get the user out of session and pass to template
+            });
+        });
 
-app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+        // route for logging out
+        app.get('/logout', function(req, res) {
+            req.logout();
+            res.redirect('/');
+        });
 
-// the callback after google has authenticated the user
-app.get('/auth/google/callback',
-				passport.authenticate('google', {
-						successRedirect : '/dashboard',
-						failureRedirect : '/'
-				}));
-};
-// route middleware to make sure a user is logged in
-function isLoggedIn(req, res, next) {
-		// if user is authenticated in the session, carry on
-		if (req.isAuthenticated())
-				return next();
+        app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
 
-		// if they aren't redirect them to the home page
-		res.redirect('/');
-}
+        // the callback after google has authenticated the user
+        app.get('/auth/google/callback',
+                passport.authenticate('google', {
+                    successRedirect : '/dashboard',
+                failureRedirect : '/'
+                }));
+    };
+    // route middleware to make sure a user is logged in
+    function isLoggedIn(req, res, next) {
+        // if user is authenticated in the session, carry on
+        if (req.isAuthenticated())
+            return next();
+
+        // if they aren't redirect them to the home page
+        res.redirect('/');
+    }
+    function logged(req, res) {
+        if (req.isAuthenticated()){
+            return true;
+        };
+    }
